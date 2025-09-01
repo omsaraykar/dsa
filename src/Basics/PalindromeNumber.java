428b69330250b64669d7a4ae0a47dd5657229ecc
omsaraykar <omsaraykar@gmail.com>
2025-09-02 00:39:52 +0530

    first commit

---
+++ src/Basics/PalindromeNumber.java
@@ -0,0 +1,17 @@
+package Basics;
+
+public class PalindromeNumber {
+    public boolean isPalindrome(int x) {
+        if (x < 0) return false;
+        int num = x;
+        int rev = 0;
+
+        while (x > 0) {
+            int lastDigit = x % 10;
+            x /= 10;
+            rev = rev * 10 + lastDigit;
+        }
+
+        return num == rev;
+    }
+}
---
+++ src/Basics/ReverseNumber.java
@@ -0,0 +1,28 @@
+package Basics;
+
+public class ReverseNumber {
+    public int reverse(int x) {
+        int rev = 0;
+        while (x!=0) {
+            int lastDigit = x%10;
+            x /= 10;
+
+            // Check for overflow
+            if (rev > Integer.MAX_VALUE/10 || (rev == Integer.MAX_VALUE/10 && lastDigit > 7)) {
+                return 0;
+            }
+            if (rev < Integer.MIN_VALUE/10 || (rev == Integer.MIN_VALUE/10 && lastDigit < -8)) {
+                return 0;
+            }
+
+            rev = rev * 10 + lastDigit;
+        }
+
+        return rev;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(Integer.MAX_VALUE); // 2147483647
+        System.out.println(Integer.MIN_VALUE); // -2147483648
+    }
+}
