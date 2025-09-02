29710eaeee603ba8c4ae2fe690bd8d94cde1b4b6
omsaraykar <omsaraykar@gmail.com>
2025-09-02 18:32:48 +0530

    print all divisors

---
+++ src/Basics/AllDivisors.java
@@ -0,0 +1,27 @@
+package Basics;
+
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class AllDivisors {
+    public static void print_divisors(int n) {
+        ArrayList<Integer> list = new ArrayList<>();
+
+        for (int i = 1; i <= Math.sqrt(n); i++) {
+            if (n % i == 0) {
+                if (n / i == i) {
+                    list.add(i);
+                } else {
+                    list.add(i);
+                    list.add(n / i);
+                }
+            }
+        }
+
+        Collections.sort(list);
+
+        for (int i: list) {
+            System.out.print(i + " ");
+        }
+    }
+}
