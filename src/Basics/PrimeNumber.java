372cd4f4d81fa4ef16ab1ea150eef921323bcdec
omsaraykar <omsaraykar@gmail.com>
2025-09-02 19:38:04 +0530

    prime number

---
+++ src/Basics/PrimeNumber.java
@@ -0,0 +1,18 @@
+package Basics;
+
+public class PrimeNumber {
+    static boolean isPrime(int n) {
+        int count = 0;
+
+        for (int i = 1; i * i <= n; i++) {
+            if (n % i == 0) {
+                count++;
+                if (n / i != i) {
+                    count++;
+                }
+            }
+        }
+
+        return count == 2;
+    }
+}
