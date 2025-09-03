df43274cbbdadc18afa5a8fec68f8d2e9acd1d83
omsaraykar <omsaraykar@gmail.com>
2025-09-04 00:07:03 +0530

    reverse array

---
+++ src/Basics/Recurssion/ArrayReverse.java
@@ -0,0 +1,22 @@
+package Basics.Recurssion;
+
+public class ArrayReverse {
+    public void reverseArray(int[] arr, int start, int end) {
+        if (start < end) {
+            int temp = arr[start];
+            arr[start] = arr[end];
+            arr[end] = temp;
+            reverseArray(arr, start + 1, end - 1);
+        }
+    }
+
+    public static void main(String[] args) {
+        ArrayReverse obj = new ArrayReverse();
+        int[] arr = new int[] {1, 2, 3, 4, 5, 6};
+        obj.reverseArray(arr, 0, 5);
+
+        for (int ele: arr) {
+            System.out.print(ele + " ");
+        }
+    }
+}
