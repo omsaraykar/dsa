84ab0390cb6b09112e151e1610ad05b2dab80fa4
omsaraykar <omsaraykar@gmail.com>
2025-09-04 00:41:27 +0530

    valid palindrome

---
+++ src/Basics/Recurssion/Printing.java
@@ -0,0 +1,28 @@
+package Basics.Recurssion;
+
+public class Printing {
+    public void oneToN(int n) {
+        if (n == 1) {
+            System.out.println(1);
+            return;
+        }
+
+        oneToN(n - 1);
+        System.out.println(n);
+    }
+
+    public void nToOne(int n) {
+        if (n == 0) {
+            return;
+        }
+
+        System.out.println(n);
+        nToOne(n - 1);
+    }
+
+    public static void main(String[] args) {
+        Printing obj = new Printing();
+        obj.oneToN(5);
+        obj.nToOne(5);
+    }
+}
---
+++ src/Strings/ValidPalindrome.java
@@ -0,0 +1,28 @@
+package Strings;
+
+public class ValidPalindrome {
+    public boolean isPalindrome(String s) {
+        s = s.toLowerCase();
+        int start = 0, end = s.length() - 1;
+
+        while (start < end) {
+            char leftChar = s.charAt(start), rightChar = s.charAt(end);
+            if (!Character.isLetterOrDigit(leftChar)) start++;
+            else if (!Character.isLetterOrDigit(rightChar)) end--;
+            else if (leftChar != rightChar) {
+                System.out.println(leftChar + " " + rightChar);
+            }
+            else {
+                start++;
+                end--;
+            }
+        }
+
+        return true;
+    }
+
+    public static void main(String[] args) {
+        ValidPalindrome obj = new ValidPalindrome();
+        System.out.println(obj.isPalindrome("A man, a plan, a canal: Panama"));
+    }
+}
