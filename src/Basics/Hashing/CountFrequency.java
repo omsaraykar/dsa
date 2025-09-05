50ec4af9406b765ddbb87caf22cf2cb23697ea00
omsaraykar <omsaraykar@gmail.com>
2025-09-05 23:49:23 +0530

    count frequencies

---
+++ src/Basics/Hashing/CountFrequency.java
@@ -0,0 +1,28 @@
+package Basics.Hashing;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+public class CountFrequency {
+    public List<List<Integer>> countFrequencies(int[] nums) {
+        List<List<Integer>> res = new ArrayList<>();
+        HashMap<Integer, Integer> map = new HashMap<>();
+
+        for (int num: nums) {
+            int freq = map.getOrDefault(num, 0);
+            map.put(num, ++freq);
+        }
+
+        for (int key: map.keySet()) {
+            res.add(new ArrayList<>(List.of(key, map.get(key))));
+        }
+
+        return res;
+    }
+
+    public static void main(String[] args) {
+        CountFrequency obj = new CountFrequency();
+        System.out.println(obj.countFrequencies(new int[] {5,5,5,5}));
+    }
+}
