d30fb1f606023e23689a78f1b8b4c00dda3d8f55
omsaraykar <omsaraykar@gmail.com>
2025-09-15 08:29:00 +0530

    pascal triangle

---
+++ src/_3_Arrays/PascalTriangle.java
@@ -0,0 +1,23 @@
+package _3_Arrays;
+
+import java.util.*;
+
+public class PascalTriangle {
+    public List<List<Integer>> generate(int numRows) {
+        List<List<Integer>> res = new ArrayList<>();
+
+        for (int i = 0; i < numRows; i++) {
+            List<Integer> list = new ArrayList<>();
+            for (int j = 0; j <= i; j++) {
+                if (j == 0 || j == i) {
+                    list.add(1);
+                } else {
+                    list.add(res.get(i - 1).get(j) + res.get(i - 1).get(j - 1));
+                }
+            }
+            res.add(list);
+        }
+
+        return res;
+    }
+}
