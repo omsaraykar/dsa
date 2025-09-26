de2976ea3b35c32cbcc314c39a2568c71cbb991a
omsaraykar <omsaraykar@gmail.com>
2025-09-26 21:01:04 +0530

    missing and repeating brute

--- src/_3_Arrays/MissingAndRepeating.java
+++ src/_3_Arrays/MissingAndRepeating.java
@@ -1,4 +1,26 @@
 package _3_Arrays;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+
 public class MissingAndRepeating {
+    ArrayList<Integer> findTwoElementBrute(int[] arr) {
+        ArrayList<Integer> ans = new ArrayList<>(2);
+        int n = arr.length;
+
+        int rep = -1, miss = -1;
+        for (int i = 1; i <= n; i++) {
+            int count = 0;
+            for (int j = 0; j < n; j++) {
+                if (i == arr[j]) count++;
+            }
+
+            if (count == 2) rep = i;
+            else if (count == 0) miss = i;
+        }
+
+        ans.add(rep);
+        ans.add(miss);
+        return ans;
+    }
 }
