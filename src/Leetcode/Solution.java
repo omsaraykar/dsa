ef0b14447ba5f5135952af18ba1569bf91074b95
omsaraykar <omsaraykar@gmail.com>
2025-09-29 23:53:03 +0530

    contest: split array

--- src/Leetcode/Solution.java
+++ src/Leetcode/Solution.java
@@ -3,58 +3,30 @@ package Leetcode;
 import java.util.*;
 
 public class Solution {
-    class Cords {
-        int x;
-        int y;
-
-        Cords(int x, int y) {
-            this.x = x;
-            this.y = y;
-        }
-        @Override
-        public boolean equals(Object o) {
-            if (this == o) return true;
-            if (!(o instanceof Cords)) return false;
-            Cords other = (Cords) o;
-            return this.x == other.x && this.y == other.y;
-        }
-
-        @Override
-        public int hashCode() {
-            return Objects.hash(x, y);
-        }
-    }
-    public int distinctPoints(String s, int k) {
-        int n = s.length();
-        HashSet<Cords> set = new HashSet<>();
-
+    public long splitArray(int[] nums) {
         int idx = 0;
-        while (idx + k <= n) {
-            int x = 0, y = 0;
-            for (int i = 0; i < s.length(); i++) {
-                if (i >= idx && i < idx + k) continue;
-                char ch = s.charAt(i);
-                if (ch == 'U') {
-                    y += 1;
-                } else if (ch == 'D') {
-                    y -= 1;
-                } else if (ch == 'L') {
-                    x -= 1;
-                } else if (ch == 'R') {
-                    x += 1;
-                }
-            }
+        while (idx < nums.length - 1 && nums[idx + 1] > nums[idx]) {
             idx++;
-            set.add(new Cords(x, y));
-            System.out.println(x + " " + y);
         }
 
-        return set.size();
-    }
+        long sumBefore = 0, sumAfter = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (i < idx) {
+                sumBefore += nums[i];
+            } else if (i > idx) {
+                sumAfter += nums[i];
+            }
+        }
 
+        long diff1 = Math.abs(sumBefore - (sumAfter + nums[idx]));
+        long diff2 = Math.abs((sumBefore + nums[idx]) - sumAfter);
+
+        return Math.min(diff1, diff2);
+    }
 
     public static void main(String[] args) {
         Solution sol = new Solution();
-        System.out.println(sol.distinctPoints("LUL", 1));;
+        long ans = sol.splitArray(new int[] {1,2,4,3});
+        System.out.println(ans);
     }
 }
