9b37648df374b2895114a69bccc72022184c8c41
Om Saraykar <omsaraykar@gmail.com>
2025-10-21 22:13:15 +0530

    middle of linked list

---
+++ src/_6_Linked_Lists/MiddleOfLL.java
@@ -0,0 +1,15 @@
+package _6_Linked_Lists;
+
+public class MiddleOfLL {
+    public ListNode middleNode(ListNode head) {
+        ListNode slow = head;
+        ListNode fast = head;
+
+        while(fast != null && fast.next != null){
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+
+        return slow;
+    }
+}
