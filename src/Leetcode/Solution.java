c1cfe5f89ae2d9a7e1a983ef85df03329b1c1fdc
Om Saraykar <omsaraykar@gmail.com>
2025-10-28 01:04:25 +0530

    maximum subarray of size k

--- src/Leetcode/Solution.java
+++ src/Leetcode/Solution.java
@@ -3,33 +3,29 @@ package Leetcode;
 import java.util.*;
 
 public class Solution {
-    public int longestBalanced(String s) {
-        int maxLen = 0;
-        for (int i = 0; i < s.length(); i++) {
-            for (int j = i; j < s.length(); j++) {
-                int[] freqArray = new int[26];
-                char ch = s.charAt(j);
-                freqArray[ch - 'a']++;
+    public long countStableSubarrays(int[] nums) {
+        int n = nums.length;
+        long[] prefix = new long[n + 1];
+        for (int i = 0; i < n; i++) {
+            prefix[i + 1] = prefix[i] + nums[i];
+        }
 
-                int equalFreq = 0; boolean balanced = true;
-                for (int freq: freqArray) {
-                    if (freq > 0) {
-                        if (equalFreq == 0) {
-                            equalFreq = freq;
-                        }
-                        else if (freq != equalFreq){
-                            balanced = false;
-                            break;
-                        }
-                    }
-                }
-                if (balanced) {
-                    maxLen = Math.max(maxLen, j - i + 1);
-                }
+        long count = 0;
+        HashMap<String, Integer> map = new HashMap<>();
+
+        for (int i = 2; i < n; i++) {
+            long nextPrefix = prefix[i - 1];
+            String nextKey = nums[i - 2] + "#" + nextPrefix;
+            map.put(nextKey, map.getOrDefault(nextKey, 0) + 1);
+
+            long targetPrefix = prefix[i] - nums[i];
+            String key = nums[i] + "#" + targetPrefix;
+            if (map.containsKey(key)) {
+                count += map.get(key);
             }
         }
 
-        return maxLen;
+        return count;
     }
 
     public static void main(String[] args) {
---
+++ src/_10_Sliding_Window/MaxSubarrayOfSizeK.java
@@ -0,0 +1,18 @@
+package _10_Sliding_Window;
+
+public class MaxSubarrayOfSizeK {
+    public int maxSubarraySum(int[] arr, int k) {
+        int windowSum = 0;
+        for (int i = 0; i < k; i++) {
+            windowSum += arr[i];
+        }
+
+        int maxSum = windowSum;
+        for (int i = k; i < arr.length; i++) {
+            windowSum += arr[i] - arr[i - k];
+            maxSum = Math.max(maxSum, windowSum);
+        }
+
+        return maxSum;
+    }
+}
